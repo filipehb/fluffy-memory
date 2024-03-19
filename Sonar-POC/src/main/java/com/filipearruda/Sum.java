68b6521e50f24c4ee04fa3959d84db30bf39e0a9
Filipe Arruda <filipehenriquester@gmail.com>
2024-03-18 22:51:48 -0300

    Update sum

--- Sonar-POC/src/main/java/com/filipearruda/Sum.java
+++ Sonar-POC/src/main/java/com/filipearruda/Sum.java
@@ -4,4 +4,16 @@ public class Sum {
     public int sum(int a, int b){
         return a + b;
     }
+
+    public double sum(double a, double b){
+        return a + b;
+    }
+
+    public float sum(float a, float b){
+        return a + b;
+    }
+
+    public long sum(long a, long b){
+        return a + b;
+    }
 }
